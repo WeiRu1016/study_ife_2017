435df5abd29c788d3c42a7a30750c7cab2e2e643
weiru <weiru@rd.tuan800.com>
2017-02-27 20:32:14 +0800

    动态数据绑定（-）

---
+++ 动态数据绑定（一）/index.js
@@ -0,0 +1,33 @@
+function Observer (obj) {
+	this.data = obj;
+	this.walk(this.data);
+}
+Observer.prototype.walk = function (data) {
+	var val;
+	for (var key in data) {
+		val = data[key];
+		if (typeof val === "object") {
+			new Observer(val);
+		} 
+		this.convert(data, key, val)
+	}
+}
+Observer.prototype.convert = function (o, key, val) {
+	var v = val;
+	Object.defineProperty(o, key, {
+		get: function(){
+			console.log("你访问了", key)
+			return v;
+		},
+		set: function(value){
+			if (typeof value === "object") {
+				new Observer(value);
+			}
+			v = value;
+			console.log("你设置了" + key + "，新的值为" + v)
+		},
+		enumerable: true,
+		configurable: true,
+		writerable: true
+	})
+}
