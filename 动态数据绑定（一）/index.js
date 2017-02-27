510f9ea6a9684cf51c0f2beca4c620f61fcb0c78
weiru <weiru@rd.tuan800.com>
2017-02-27 21:32:11 +0800

    修改

--- 动态数据绑定（一）/index.js
+++ 动态数据绑定（一）/index.js
@@ -2,6 +2,9 @@ function Observer (obj) {
 	this.data = obj;
 	this.walk(this.data);
 }
+Observer.prototype.watch = function (key, callback) {
+	this.data[key].watch(callback);
+}
 Observer.prototype.walk = function (data) {
 	var val;
 	for (var key in data) {
@@ -14,6 +17,7 @@ Observer.prototype.walk = function (data) {
 }
 Observer.prototype.convert = function (o, key, val) {
 	var v = val;
+	var pubSub = new PubSub();
 	Object.defineProperty(o, key, {
 		get: function(){
 			console.log("你访问了", key)
@@ -25,9 +29,28 @@ Observer.prototype.convert = function (o, key, val) {
 			}
 			v = value;
 			console.log("你设置了" + key + "，新的值为" + v)
+			pubSub.emit('change');
+		},
+		watch: function(callback) {
+			pubSub.on('change', callback)
 		},
 		enumerable: true,
 		configurable: true,
 		writerable: true
 	})
+}
+var PubSub = function () {
+	this.handlers = {}
+}
+PubSub.prototype.on = function (eventType, handler) {
+	if (!this.handlers[eventType]) {
+		this.handlers[eventType] = []
+	}
+	this.handlers[eventType].push(handler);
+}
+PubSub.prototype.emit = function (eventType) {
+	var handlers = this.handlers[eventType];
+	for (var i = 0,l = handlers.length;i < l;i ++) {
+		handlers[i]();
+	}
 }
